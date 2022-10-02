da19bb0ebb459dc2294c4c14a4fae76c6585149b
Pramodhkumar <114848124+MPramodhkumar@users.noreply.github.com>
2022-10-02 18:55:25 +0530

    CODE

---
+++ 24, LAN ALLOCATION/Solution.py
@@ -0,0 +1,11 @@
+#python
+x=int(input("Enter x\n"))
+y=int(input("Enter y\n"))
+z=int(input("Enter z\n"))
+if x<y  and x<z:
+    print("L1 has the minimal seating capacity")
+elif y<x and y<z:
+    print("L2 has the minimal seating capacity")
+else:
+     print("L3 has the minimal seating capacity")
+    
