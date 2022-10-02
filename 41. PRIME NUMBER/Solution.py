9fa58480e025b4e90e167f831620db3d0d02aac9
Pramodhkumar <114848124+MPramodhkumar@users.noreply.github.com>
2022-10-02 19:15:40 +0530

    CODE

---
+++ 41. PRIME NUMBER/Solution.py
@@ -0,0 +1,12 @@
+#python
+num=int(input())
+if (num>1):
+    for i in range(2,num):
+        if (num%i)==0:
+            print("Not a prime")
+            break
+    else:
+        print("prime number")
+        
+else:
+    print("not a prime")
