2525d6e18a76429a0861689f04603c01a9bde4b3
Pramodhkumar <114848124+MPramodhkumar@users.noreply.github.com>
2022-10-02 19:07:46 +0530

    CODE

---
+++ 33. FACTORIAL OR NOT/Solution.py
@@ -0,0 +1,28 @@
+def isFactorial(n) :
+    i = 1;
+    while(True) :
+         
+        if (n % i == 0) :
+            n //= i;
+             
+        else :
+            break;
+             
+        i += 1;
+ 
+    if (n == 1) :
+        return True;
+     
+    else :
+        return False;
+ 
+# Driver Code
+if __name__ == "__main__" :
+    n = int(input())
+    ans = isFactorial(n);
+     
+    if (ans == 1) :
+        print("Yes");
+ 
+    else :
+        print("No");
