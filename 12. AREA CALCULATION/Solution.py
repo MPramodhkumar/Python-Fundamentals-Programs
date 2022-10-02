54396cc040bd5547cb6253a22a1d659157293e13
Pramodhkumar <114848124+MPramodhkumar@users.noreply.github.com>
2022-10-02 18:40:48 +0530

    CODE

---
+++ 12. AREA CALCULATION/Solution.py
@@ -0,0 +1,17 @@
+#python
+pi=3.14
+x=int(input())
+print("\n")
+y=int(input())
+print("\n")
+z=int(input())
+print("\n")
+w=float(input())
+a=x**2
+b=y*z
+c=pi * w*w
+print(a)
+print("\n")
+print(b)
+print("\n")
+print("%.2f"%c)
